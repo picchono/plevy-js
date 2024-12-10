0383e48001cb54956ad9447532b41ac4cc115b56
picchono <72742974+picchono@users.noreply.github.com>
2024-12-10 19:19:11 +0100

    start

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <div>test</div>
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
@@ -18,7 +19,7 @@ export default function Home() {
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
               app/page.tsx
             </code>
-            .
+            . Worked!
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
---
+++ gridstack.config.js
@@ -0,0 +1,19 @@
+export default {
+    // Set the grid width and height
+    width: 1200,
+    height: 800,
+  
+    // Enable drag-and-drop functionality
+    draggable: false,
+  
+    // Enable resizable grid items
+    resizable: false,
+  
+    // Define the grid item styles
+    itemStyles: {
+      '.grid-stack-item-content': {
+        backgroundColor: '#18BC9C',
+      },
+    },
+  };
+  
